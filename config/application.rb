b2794cfc92b10036153a203c25f87d25423cb6fd
Jon Wexler <jwexman@gmail.com>
2016-02-01 23:10:51 -0500

    sass preferred

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module SocketsDemo
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # makes all sass scaffolds with sass instead of CSS
+    config.sass.preferred_syntax = :sass
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
